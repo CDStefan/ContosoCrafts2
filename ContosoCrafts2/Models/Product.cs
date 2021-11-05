b84aa1d30f8b6f1a531ce4e346a776f9ded38ed1
Stefan Caravelea <caraveleastefan@gmail.com>
2021-11-05 17:20:14 +0200

    Add project files.

---
+++ ContosoCrafts2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.Website2.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string maker { get; set; }
+        [JsonPropertyName("img")]
+        public string img { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int[] Ratings { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+           
+
+       
+            
+
+    }
+}
